c727a5f046c5ca0fe2a8c14f315020112d9ec6f7
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-05-24 23:39:49 +0530

    Added: Masonry grid for notes grid

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -1,17 +1,43 @@
-import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import React, { useContext } from "react";
+import Masonry from "react-masonry-css";
 import { NoteCard } from "../components/NoteCard";
 import { NotesContext } from "../context/global";
+
+const useStyle = makeStyles((theme) => ({
+  notesGrid: {
+    display: "flex",
+    marginLeft: -theme.spacing(2),
+    width: "auto",
+  },
+  notesColumnGrid: {
+    paddingLeft: theme.spacing(2),
+    backgroundClip: "padding-box",
+  },
+  divSpaced: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
 export const View = () => {
   const { notes } = useContext(NotesContext) as NotesContextType;
-
+  const classes = useStyle();
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
   return (
-    <Grid container spacing={3}>
+    <Masonry
+      breakpointCols={breakpoints}
+      className={classes.notesGrid}
+      columnClassName={classes.notesColumnGrid}
+    >
       {notes.map((note) => (
-        <Grid key={note.id} item xs={12} sm={6} md={4} lg={3}>
+        <div key={note.id} className={classes.divSpaced}>
           <NoteCard note={note} />
-        </Grid>
+        </div>
       ))}
-    </Grid>
+    </Masonry>
   );
 };
