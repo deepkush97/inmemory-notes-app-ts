6f724d618c29ddefdfd7ea4834674e68fc18f7bb
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-05-24 20:38:38 +0530

    Added: Create Page

---
+++ src/context/global.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+
+export type CategoryType = "todo" | "reminder" | "personal" | "work";
+
+export interface ICreateNote {
+  title: string;
+  category: CategoryType;
+  details: string;
+}
+
+export interface INote extends ICreateNote {
+  id: number;
+}
+
+export type NotesContextType = {
+  notes: INote[];
+  saveNote: (todo: ICreateNote) => void;
+  deleteNote: (id: number) => void;
+};
+
+export const NotesContext = React.createContext<NotesContextType | null>(null);
+
+export const NotesProvider: React.FC<React.ReactNode> = ({ children }) => {
+  const [notes, setNotes] = React.useState<INote[]>([
+    {
+      id: 1,
+      title: "note 1",
+      details: "this is a description",
+      category: "personal",
+    },
+    {
+      id: 2,
+      title: "note 2",
+      details: "this is a description",
+      category: "work",
+    },
+    {
+      id: 3,
+      title: "note 3",
+      details: "this is a description",
+      category: "reminder",
+    },
+    {
+      id: 4,
+      title: "note 4",
+      details: "this is a description",
+      category: "todo",
+    },
+  ]);
+
+  const saveNote = ({ category, details, title }: ICreateNote) => {
+    const newNote: INote = {
+      id: Math.random(),
+      title,
+      details,
+      category,
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (id: number) => {
+    setNotes((prev) => prev.filter((a) => a.id !== id));
+  };
+
+  return (
+    <NotesContext.Provider value={{ notes, saveNote, deleteNote }}>
+      {children}
+    </NotesContext.Provider>
+  );
+};
---
+++ src/pages/create.tsx
@@ -0,0 +1,130 @@
+import React, { useState, FormEvent, useContext } from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import { useHistory } from "react-router-dom";
+import {
+  NotesContext,
+  NotesContextType,
+  CategoryType,
+} from "../context/global";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
+
+export const Create = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState<CategoryType>("todo");
+
+  const { saveNote } = useContext(NotesContext) as NotesContextType;
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title === "") {
+      setTitleError(true);
+    }
+    if (details === "") {
+      setDetailsError(true);
+    }
+    if (title && details) {
+      saveNote({ title, details, category });
+      history.push("/");
+    }
+  };
+
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          className={classes.field}
+          onChange={(e) => setTitle(e.target.value)}
+          label="Note Title"
+          variant="outlined"
+          // color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          className={classes.field}
+          onChange={(e) => setDetails(e.target.value)}
+          label="Details"
+          variant="outlined"
+          // color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value as CategoryType)}
+          >
+            <FormControlLabel
+              value="personal"
+              control={<Radio color="primary" />}
+              label="Personal"
+            />
+            <FormControlLabel
+              value="todo"
+              control={<Radio color="primary" />}
+              label="Todo"
+            />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio color="primary" />}
+              label="Reminder"
+            />
+            <FormControlLabel
+              value="work"
+              control={<Radio color="primary" />}
+              label="Work"
+            />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+          type="submit"
+          // color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+};
